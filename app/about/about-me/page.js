8dd01cf157fd84136aafdde34ac91e572c907aa2
=xzsa654 <=xzsa654@gmail.com>
2024-12-16 14:17:47 +0800

    layout 巢狀嵌套

---
+++ app/about/about-me/page.js
@@ -0,0 +1,11 @@
+'use client'
+
+import React, { useState, useEffect } from 'react'
+
+export default function AboutMePage(props) {
+  return (
+    <>
+      <div>AboutMe Pag1231e</div>
+    </>
+  )
+}
